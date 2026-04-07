d24faca52e023aa79335d064bb16f582b2d5a6bc
Oleksandr Koniukh <kolexandr1983@gmail.com>
2026-04-07 13:30:17 +0300

    started frontend / (made some crud)

--- backend/src/api/habits.ts
+++ backend/src/api/habits.ts
@@ -2,32 +2,46 @@ import express, {Request, Response, NextFunction, Router} from "express";
 import {prisma} from "../../prisma.ts"
 import {z} from "zod";
 import { ScheduleType, HabitStatus } from "../../generated/prisma/enums.ts";
+import { id } from "zod/locales";
 
 const router = Router();
-router.use(express.json())
-enum schedule {
-  "DAILY",
-  "WEEKLY",
-  "CUSTOM"
-}
-
-enum status{
-  "ACTIVE",
-  "ARCHIVE"
-}
 
 
 const habitSchema = z.object({
   name: z.string().min(3),
   description: z.string().min(3).optional(),
-  ScheduleType: z.enum(ScheduleType),
-  HabitStatus: z.enum(HabitStatus),
+  scheduleType: z.enum(ScheduleType),
+  habitStatus: z.enum(HabitStatus),
   targetPerPeriod: z.number().int().positive().default(1),
-  endDate: z.iso.datetime().optional(),
+  endDate: z.string().datetime().optional(),
+  userId: z.string(),
 });
 
+type HabitDto = z.output<typeof habitSchema>;
+
+router.get("/:id", async (req: Request, res: Response) => {
+  const id_habits = req.params.id;
+  try {
+    const habit = await prisma.habit.findUnique({where: {id: id_habits}});
+    res.json(habit);
+  } catch (error) {
+    res.status(500).send("Error retrieving habit.");
+  }
+});
+
+router.post("/:id", async (req: Request, res: Response) => {
+  const result = habitSchema.safeParse(req.body);
+  if (!result.success){
+    return res.status(400).send("Invalid information.");
+  }
+
+  const dataHabit : HabitDto = result.data;
 
 
-router.get("/habits/:id", (req: Request, res: Response) => {
-  // const 
+  try {
+    const habit = await prisma.habit.create({data: dataHabit});
+    res.status(201).json(habit);
+  } catch (error) {
+    
+  }
 });
---
+++ frontend/habit-tracker/src/components/Regpage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Regpage = () => {
+  
+  return (
+    <div>
+          
+    </div>
+  )
+}
+
+export default Regpage
